40f9086597a6d4e3fc09f00969aa0857133622bb
John Bonjour <whitewolfzhang@gmail.com>
2023-12-15 10:22:54 -0800

    1. modified dead-zones 2. modified PID 3. added period ticks for raw and custom 4. added conversion coefficient from radians to ticks

--- src/main/java/frc/robot/Constants.kt
+++ src/main/java/frc/robot/Constants.kt
@@ -1,10 +1,13 @@
 package frc.robot
 
+import kotlin.math.PI
+
 object Constants {
     const val CAN_TIMEOUT_MS = 10
 
     object DriveSystemConst {
         const val DEGREES2TICKS = 2048.0 / 360.0
+        const val RADIANS2TICKS = 2048.0 / (2 * PI)
 
         const val CURRENT_LIMIT_AMPS = 80.0
         const val CURRENT_LIMIT_DELAY = 0.25
@@ -15,9 +18,14 @@ object Constants {
 //        const val POWER_F = 1023.0 / 20660.0
 //        const val POWER_I_ZONE = 300.0
 
-        const val THETA_P = 0.1
-        const val THETA_I = 0.00001
-        const val THETA_D = 1.0
+        const val THETA_P = 1.0
+        const val THETA_I = 0.0
+        const val THETA_D = 0.0
+
+        const val THETA_GEAR_RATIO = 1
+
+        const val TALONFX_PERIOD_TICKS = 2048.0
+        const val THETAMOTOR_PERIOD_TICKS = TALONFX_PERIOD_TICKS * 6.25
     }
     object ControllerConst {
         const val DRIVE_JOYSTICK_PORT = 0
@@ -26,8 +34,10 @@ object Constants {
         const val Y_CHANNEL = 1
         const val Z_CHANNEL = 4
 
-        const val X_DEAD_ZONE = 0.2
-        const val Y_DEAD_ZONE = 0.2
-        const val Z_DEAD_ZONE = 0.2
+        const val X_DEAD_ZONE = 0.05
+        const val Y_DEAD_ZONE = 0.05
+        const val Z_DEAD_ZONE = 0.05
+
+        const val R_DEAD_ZONE = 0.05
     }
 }
